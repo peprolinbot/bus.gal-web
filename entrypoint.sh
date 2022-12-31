8956ab5ca9ca1a6d5abc39432c5ffdf3bcdc8a4b
Pedro Rey Anca <peprolinbot@noreply.codeberg.org>
2022-12-31 13:03:49 +0000

    Actualizar 'entrypoint.sh'

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gunicorn busGal_Django.wsgi -b 0.0.0.0:8000 --timeout 0 &
+gunicorn busGal_Django.wsgi -b 0.0.0.0:8000 --workers $GUNICORN_WORKERS --timeout $GUNICORN_TIMEOUT &
 
 nginx -g 'daemon off;'
