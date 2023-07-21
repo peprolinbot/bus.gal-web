d1e9a2d7d145f2a848846cd75373cb49ba731d39
Pedro Rey <contact@peprolinbot.com>
2023-07-21 16:31:14 +0200

    Add scheduled index updates

---
+++ buses/jobs.py
@@ -0,0 +1,39 @@
+from schedule import Scheduler
+import threading
+import time
+
+from .utils.stops_cache import stops_cache
+
+def run_continuously(self, interval=1):
+    """Continuously run, while executing pending jobs at each elapsed
+    time interval.
+    @return cease_continuous_run: threading.Event which can be set to
+    cease continuous run.
+    Please note that it is *intended behavior that run_continuously()
+    does not run missed jobs*. For example, if you've registered a job
+    that should run every minute and you set a continuous run interval
+    of one hour then your job won't be run 60 times at each interval but
+    only once.
+    """
+
+    cease_continuous_run = threading.Event()
+
+    class ScheduleThread(threading.Thread):
+
+        @classmethod
+        def run(cls):
+            while not cease_continuous_run.is_set():
+                self.run_pending()
+                time.sleep(interval)
+
+    continuous_thread = ScheduleThread()
+    continuous_thread.setDaemon(True)
+    continuous_thread.start()
+    return cease_continuous_run
+
+Scheduler.run_continuously = run_continuously
+
+def start_scheduler():
+    scheduler = Scheduler()
+    scheduler.every().minute.do(stops_cache.update_index)
+    scheduler.run_continuously()
--- buses/utils/stops_cache.py
+++ buses/utils/stops_cache.py
@@ -5,6 +5,7 @@ from ..templatetags.buses_extras import form_stop_string
 from whoosh.fields import Schema, STORED, NUMERIC, TEXT
 from whoosh.index import create_in, open_dir
 from whoosh.qparser import QueryParser
+from whoosh import writing
 
 import os.path
 
@@ -20,27 +21,30 @@ class StopsCache():
 
         if not os.path.exists(self.cache_dir):
             os.mkdir(self.cache_dir)
-            self.update_index()
+            create_in(self.cache_dir, self.schema)
         else:
             self.ix = open_dir(self.cache_dir)
 
-    def update_index(self):
-        self.ix = create_in(self.cache_dir, self.schema)
+        self.update_index()
 
+    def update_index(self):
         stops = get_all_stops()
 
-        writer = self.ix.writer()
-        for stop in stops:
-            writer.add_document(id=form_stop_string(
-                stop), group_type=stop.group_type, name=stop.name)
-        writer.commit()
+        with self.ix.writer() as writer:
+            for stop in stops:
+                writer.add_document(id=form_stop_string(
+                    stop), group_type=stop.group_type, name=stop.name)
+
+            # Clears all existing documents, only the ones added in this writer are left
+            writer.mergetype = writing.CLEAR
 
     def autocomplete_search(self, text):
         with self.ix.searcher() as searcher:
             name_query = QueryParser("name", stops_cache.ix.schema).parse(text)
             results = searcher.search(name_query, sortedby="group_type")
 
-            group_type_icons = ["🏢", "🎖️", "📍", "📌"]  # Imagine it's one-indexed
+            # Imagine it's one-indexed
+            group_type_icons = ["🏢", "🎖️", "📍", "📌"]
             return [{"id": r["id"], "text": f"{group_type_icons[r['group_type']-1]} {r['name']}"}
                     for r in results]
 
