3e593dd7aa8fec8e6de8d4cc55405a2d746e6528
peprolinbot <personal@peprolinbot.com>
2023-11-04 12:56:43 +0100

    --run-syncdb migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python manage.py migrate
+python manage.py migrate --run-syncdb
 
 gunicorn busGal_Django.wsgi --preload -b 0.0.0.0:8000 --workers ${GUNICORN_WORKERS:-3} --timeout ${GUNICORN_TIMEOUT:-30} &
 python3 manage.py qcluster &
