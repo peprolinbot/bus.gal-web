5728bb789b30800f3fa74f4434b909738d686f0a
peprolinbot <personal@peprolinbot.com>
2023-11-04 17:09:27 +0100

    Don't run Django-q on migration

--- buses/tasks.py
+++ buses/tasks.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 
+import sys
+
 from django_q.models import Schedule
 
 
 def setup_tasks():
-    if not settings.IS_DOCKER_BUILD:
+    if not settings.IS_DOCKER_BUILD and not 'migrate' in sys.argv:
         Schedule.objects.get_or_create(name="Update the bus stops search index",
                                        func='buses.utils.stops_cache.stops_cache.update_index',
                                        schedule_type=Schedule.MINUTES,
