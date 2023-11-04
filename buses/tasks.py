56b1258e0511fcda6e8093899f08469a19f26153
peprolinbot <personal@peprolinbot.com>
2023-11-04 16:11:33 +0100

    Fix docker build

--- buses/tasks.py
+++ buses/tasks.py
@@ -1,8 +1,11 @@
+from django.conf import settings
+
 from django_q.models import Schedule
 
 
 def setup_tasks():
-    Schedule.objects.get_or_create(name="Update the bus stops search index",
-                                   func='buses.utils.stops_cache.stops_cache.update_index',
-                                   schedule_type=Schedule.MINUTES,
-                                   minutes=2)
+    if not settings.IS_DOCKER_BUILD:
+        Schedule.objects.get_or_create(name="Update the bus stops search index",
+                                       func='buses.utils.stops_cache.stops_cache.update_index',
+                                       schedule_type=Schedule.MINUTES,
+                                       minutes=2)
--- xenovaping/tasks.py
+++ xenovaping/tasks.py
@@ -1,7 +1,10 @@
+from django.conf import settings
+
 from django_q.models import Schedule
 
 
 def setup_tasks():
-    Schedule.objects.get_or_create(name="Check and send the neccesary 'pending refund' notifications",
-                                   func='xenovaping.utils.notifications.run_checks',
-                                   schedule_type=Schedule.DAILY)
+    if not settings.IS_DOCKER_BUILD:
+        Schedule.objects.get_or_create(name="Check and send the neccesary 'pending refund' notifications",
+                                       func='xenovaping.utils.notifications.run_checks',
+                                       schedule_type=Schedule.DAILY)
